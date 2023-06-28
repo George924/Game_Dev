17e2285ef734cead927ef4f771c49c15c6107d20
Dragos Tache <dragos.tache@interdigital.ro>
2023-06-28 21:47:24 +0300

    fixes

--- curs_01/hilo/hilo.js
+++ curs_01/hilo/hilo.js
@@ -1,4 +1,3 @@
-
 let numere = [3, 4, 9];
 numere.push(7);
 console.log(numere);
@@ -9,10 +8,10 @@ numere.splice(3, 0, 14);
 console.log(numere);
 
 
-let cardTypes = ['clubs', 'diamonds', 'hearts','spades'];
+let cardTypes = ['clubs', 'diamonds', 'hearts', 'spades'];
 let pachet = {
     'clubs': [], //
-    'diamonds':[],
+    'diamonds': [],
     'hearts': [],
     'spades': []
 };
@@ -20,76 +19,94 @@ let score = 0;
 let vecheaCarte = 0;
 
 function adaugaCartiInPachet(nrPachete = 1) {
-    for (i = 0; i < cardTypes.length; i++)  { // index card type
-        // i // index (0, 1, 2, 3)
-        // cardTypes[i] // 'clubs', ...
-        // masina['model'] 
-        //masina.model == masina['model']
-        //masina[model]
-        
-        pachet[cardTypes[i]]
-     for ( let j = 2; j <= 14; j++) { // card number
-        pachet[cardTypes[i]] [pachet[cardTypes[i]]]=j;
-
-     }
+    for (let i = 0; i < nrPachete; i++) { //for number of packets
+        for (let k = 0; k <= 3; k++) { // for card types
+            let cardType = cardTypes[k];
+            for (let j = 2; j <= 14; j++) { //for each card number
+                let listOfCards = pachet[cardType];
+                listOfCards.push(j);
+                pachet[cardType] = listOfCards;
+            }
+        }
     }
+    console.log('test');
     console.log(pachet);
 }
 
 
 function incarcare() {
+    console.log('test');
 
-    
-    adaugaCartiInPachet;
+
+    adaugaCartiInPachet();
     // adaugare carte noua;
-     nouaCarte = schimbaCarte();
-     // ar urma comparatie ()
-     vecheaCarte = nouaCarte;
+    nouaCarte = schimbaCarte();
+    // ar urma comparatie ()
+    vecheaCarte = nouaCarte;
     // setare scor zero
     afiseazaScor();
     //...
 }
-function afiseazaScor(){
+
+function afiseazaScor() {
     const divScore = document.getElementById('score');
-   const spanScore = divScore.getElementsByTagName('span')[0];
-   spanScore.textContent = score;
+    const spanScore = divScore.getElementsByTagName('span')[0];
+    spanScore.textContent = score;
 }
+
 function showFace(happy) {
-    const divIdToShow = 'face-' + (happy ? 'happy' : 'sad'); //short
-    const divIdToHide = 'face-' + (!happy ? 'happy' : 'sad'); // short
-    document.getElementById(divIdToShow).style.dusplay = 'block';
-    document.getElementById(divIdToHide).style.dusplay = 'none';
+    if ( happy) {
+        document.getElementById('face-happy').style.display= 'block';
+        document.getElementById('face-sad').style.display= 'none';
+    }
+
+    else  {
+        document.getElementById('face-sad').style.display= 'block';
+        document.getElementById('face-happy').style.display= 'none';
+
+      
+    }
 
 
 }
 
 function schimbaCarte() {
-   const indexType =  Math.floor(Math.random() * 4); 
-   const cardNumber = Math.floor(Math.random() * 13 + 2); // [0;14) * 13 =>  [0;13) + 2 => [2; 13) ; [2; 15)
-   const pozitie = pachet[cardTypes[indexType]].indexOf(cardNumber);
-   const fileName =  'cards/' + cardTypes[indexType] + '_' + cardNumber + '.svg';
-   if (pozitie == -1) {
-    console.log('Am obtinut ' + fileName + 'si nu e in pachet')
-    schimbaCarte();
-    return;
-   }
-   pachet[cardTypes[indexType]].splice(pozitie, 1);
+    const indexType = Math.floor(Math.random() * 4);
+    const cardNumber = Math.floor(Math.random() * 13 + 2); // [0;14) * 13 =>  [0;13) + 2 => [2; 13) ; [2; 15)
+    const pozitie = pachet[cardTypes[indexType]].indexOf(cardNumber);
+    const fileName = 'cards/' + cardTypes[indexType] + '_' + cardNumber + '.svg';
+    if (pozitie == -1) {
+        console.log('Am obtinut ' + fileName + 'si nu e in pachet')
+        schimbaCarte();
+        return;
+    }
+    pachet[cardTypes[indexType]].splice(pozitie, 1);
 
 
-   
 
-   //lastCardNumber = cardNumber;
-   
-   console.log(fileName);
-   const imgElement = document.querySelector('#card > img');
 
-    imgElement.setAttribute('src' , fileName);
-    //return cardNumber;
-    
+    //lastCardNumber = cardNumber;
+
+    console.log(fileName);
+    const imgElement = document.querySelector('#card > img');
+
+    imgElement.setAttribute('src', fileName);
+    return cardNumber;
+
 }
+
+function change(higher){
+    if(higher){
+        this.higher();
+    } else {
+        lower();
+    }
+}
+
 function higher() {
-     nouaCarte = schimbaCarte();
-     console.log(typeof nouaCarte);
+    nouaCarte = schimbaCarte();
+    console.log('typeof nouaCarte');
+    console.log(typeof nouaCarte);
     if (typeof nouaCarte !== 'number') return;
     if (nouaCarte >= vecheaCarte) {
         score++;
@@ -105,13 +122,13 @@ function higher() {
 
 }
 
-const lower = function() {
-    
-    nouaCarte =schimbaCarte();
+const lower = function () {
+
+    nouaCarte = schimbaCarte();
     if (typeof nouaCarte !== 'number') return;
     if (nouaCarte <= vecheaCarte) {
 
-    
+
         score++;
         showFace(true);
         afiseazaScor();
